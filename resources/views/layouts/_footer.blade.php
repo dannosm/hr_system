c15b2bfb7bb49bb0baf34a841a87f3124dde165e
danno sm <dannoshaprilm@gmail.com>
2020-04-11 15:37:35 +0700

    update aleready

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,14 +1,8 @@
-<div class="container-fluid" >
+<!-- <div class="container-fluid" >
                                         <div class="row">
                                             <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
                                                Copyright © 2019 HR System <a href="javascript:void(0)"></a>.
                                             </div>
-                                            <!-- <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
-                                                <div class="text-md-right footer-links d-none d-sm-block">
-                                                    <a href="javascript: void(0);">About</a>
-                                                    <a href="javascript: void(0);">Support</a>
-                                                    <a href="javascript: void(0);">Contact Us</a>
-                                                </div>
-                                            </div> -->
+                                           
                                         </div>
-                                    </div>
+                                    </div> -->
